2037d2cf714e52e8ae092c0e0baf999e20a6f409
Ivan Tavarez <ivantavarez2@gmail.com>
2023-10-30 14:18:10 -0400

    Created multiple classes

---
+++ src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,72 @@
+package com.pluralsight;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+
+    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getOverTimeHours() {
+        double total = 0;
+        if(this.hoursWorked > 40) {
+            total = (this.hoursWorked - 40) * (this.payRate *= 1.5);
+        }
+        return total;
+    }
+
+    public double getTotalPay() {
+        double hoursWorked1 = getHoursWorked();
+        double overTimeWorked = getOverTimeHours();
+
+        return (hoursWorked1 * this.payRate) + overTimeWorked;
+    }
+}
