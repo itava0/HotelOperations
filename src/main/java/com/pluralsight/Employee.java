48dd343d2a6606e130f359f4f953f7bcd4a5fee3
Ivan Tavarez <ivantavarez2@gmail.com>
2023-11-01 11:17:02 -0400

    Created employee class

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,25 +1,79 @@
 package com.pluralsight;
 
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.time.*;
+
 public class Employee {
-    private int employeeId;
+    static Scanner myScanner = new Scanner(System.in);
+    static LocalDateTime timeNow = LocalDateTime.now();
+    static DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd h:mm:a");
+    private long employeeId;
     private String name;
     private String department;
     private double payRate;
     private double hoursWorked;
+    private double totalPay;
+    private double totalHours;
+    private double regularsHours;
+    private double overtimeHours;
+    private double punchIn;
+    private double punchOut;
+    static double decimalTime = timeNow.getHour() + (timeNow.getMinute() / 60);
 
-    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
+
+    public Employee(long employeeId, String name, String department, double payRate, double hoursWorked, double totalPay, double totalHours, double regularsHours, double overtimeHours, double punchIn, double punchOut) {
         this.employeeId = employeeId;
         this.name = name;
         this.department = department;
         this.payRate = payRate;
         this.hoursWorked = hoursWorked;
+        this.totalPay = totalPay;
+        this.totalHours = totalHours;
+        this.regularsHours = regularsHours;
+        this.overtimeHours = overtimeHours;
+        this.punchIn = punchIn;
+        this.punchOut = punchOut;
     }
 
-    public int getEmployeeId() {
+    public static void punchTimeCard() {
+        while (true) {
+            System.out.println("""
+                    Please choose one of the following.
+                    1) Check In
+                    2) Check Out
+                    3) Exit
+                    """);
+            String userChoice = myScanner.nextLine();
+            switch (userChoice) {
+                case "1":
+                    punchIn();
+                    break;
+                case "2":
+                    punchOut();
+                    break;
+                case "3":
+                    System.exit(0);
+                default:
+                    System.out.println("Choose 1 or 2 ");
+
+            }
+        }
+
+    }
+    public static void punchIn(double overtimeHours){}
+    public static void punchIn(){
+        System.out.println("Welcome, you've clocked in at " + timeFormat.format(timeNow) + " " + decimalTime);
+    }
+    public static void punchOut(double overtimeHours){}
+    public static void punchOut(){
+        System.out.println("Goodbye, you've clocked out at  " + timeFormat.format(timeNow) + " " + decimalTime);
+    }
+    public long getEmployeeId() {
         return employeeId;
     }
 
-    public void setEmployeeId(int employeeId) {
+    public void setEmployeeId(long employeeId) {
         this.employeeId = employeeId;
     }
 
@@ -55,18 +109,39 @@ public class Employee {
         this.hoursWorked = hoursWorked;
     }
 
-    public double getOverTimeHours() {
-        double total = 0;
-        if(this.hoursWorked > 40) {
-            total = (this.hoursWorked - 40) * (this.payRate *= 1.5);
+    public double getTotalPay() {
+        if (overtimeHours < 0) {
+            return regularsHours * payRate;
+        } else {
+            return (regularsHours * payRate) + (overtimeHours * payRate * 1.5);
         }
-        return total;
     }
 
-    public double getTotalPay() {
-        double hoursWorked1 = getHoursWorked();
-        double overTimeWorked = getOverTimeHours();
+    public void setTotalPay(double totalPay) {
+        this.totalPay = totalPay;
+    }
 
-        return (hoursWorked1 * this.payRate) + overTimeWorked;
+    public double getTotalHours() {
+        return regularsHours + overtimeHours;
+    }
+
+    public void setTotalHours(double totalHours) {
+        this.totalHours = totalHours;
+    }
+
+    public double getRegularsHours() {
+        return regularsHours;
+    }
+
+    public void setRegularsHours(double regularsHours) {
+        this.regularsHours = regularsHours;
+    }
+
+    public double getOvertimeHours() {
+        return overtimeHours;
+    }
+
+    public void setOvertimeHours(double overtimeHours) {
+        this.overtimeHours = overtimeHours;
     }
 }
